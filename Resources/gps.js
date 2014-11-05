3b6f2d757e157a1b896425c7b7b26c5cff0e7305
PRuddiman <PRuddiman062@g.rwu.edu>
2014-11-05 14:51:11 -0500

    Fixed X, Y positioning system and started GPS

---
+++ Resources/gps.js
@@ -0,0 +1,7 @@
+/* File: gps.js
+ * Organization: Technical Entrepreneurs <Tech-Es>
+ * Contributors: 
+ * Date: 10/28/2014
+ * Desc: gps connector, handles grabing gps coordinates and decyphering pixel to gps location. 
+ * requires: lib.config, lib.data;
+ */
